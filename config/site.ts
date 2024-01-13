3e517fbe277c583829169c5c7c81587b70560ec9
carlobortolan <carlobortolan@gmail.com>
2024-01-13 17:29:12 +0100

    Update developers url

--- config/site.ts
+++ config/site.ts
@@ -5,11 +5,11 @@ export const siteConfig: SiteConfig = {
   description:
     "Solutions that brain-boost HR.",
   url: "https://embloy.com",
-  api_url: "https://embloy-core.onrender.com/api/v0",
+  api_url: "https://api.embloy.com/api/v0",
   // api_url: "http://localhost:3000/api/v0",
   ogImage: "https://embloy.com/assets/neu_banner-879ef82b.png",
   links: {
-    developer: "https://developer.embloy.com",
+    developer: "https://developers.embloy.com",
     about: "https://about.embloy.com",
     genius: "https://genius.embloy.com",
     twitter: "https://twitter.com/embloy",
