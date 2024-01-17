eaf9c4e6e1d9b3785de8a6de028484eefba82525
carlobortolan <carlobortolan@gmail.com>
2024-01-17 17:47:04 +0100

    Updated subscription information

--- app/(marketing)/pricing/page.tsx
+++ app/(marketing)/pricing/page.tsx
@@ -16,39 +16,41 @@ export default function PricingPage() {
           Simple, transparent pricing
         </h2>
         <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
-          Unlock all features including unlimited posts for your blog.
+          For applicants, Embloy is 100% free.</p>
+        <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+          Job providers can request a basic subscription for an application if they want to verify that a user is "real" in which case the applicant will have to subscribe to Embloy-Free.
         </p>
       </div>
       <div className="grid w-full items-start gap-10 rounded-lg border p-10 md:grid-cols-[1fr_200px]">
         <div className="grid gap-6">
           <h3 className="text-xl font-bold sm:text-2xl">
-            What&apos;s included in the PRO plan
+            What&apos;s included in EMBLOY FREE
           </h3>
           <ul className="grid gap-3 text-sm text-muted-foreground sm:grid-cols-2">
             <li className="flex items-center">
-              <Icons.check className="mr-2 h-4 w-4" /> Unlimited Posts
+              <Icons.check className="mr-2 h-4 w-4" /> One-Click-Application
             </li>
             <li className="flex items-center">
-              <Icons.check className="mr-2 h-4 w-4" /> Unlimited Users
+              <Icons.check className="mr-2 h-4 w-4" /> Apply to as many jobs as you want
             </li>
 
             <li className="flex items-center">
-              <Icons.check className="mr-2 h-4 w-4" /> Custom domain
+              <Icons.check className="mr-2 h-4 w-4" /> Upload CVs, extra documents, etc.
             </li>
             <li className="flex items-center">
-              <Icons.check className="mr-2 h-4 w-4" /> Dashboard Analytics
+              <Icons.check className="mr-2 h-4 w-4" /> Share open job positions
             </li>
             <li className="flex items-center">
-              <Icons.check className="mr-2 h-4 w-4" /> Access to Discord
+              <Icons.check className="mr-2 h-4 w-4" /> Create three job postings
             </li>
             <li className="flex items-center">
-              <Icons.check className="mr-2 h-4 w-4" /> Premium Support
+              <Icons.check className="mr-2 h-4 w-4" /> No ads
             </li>
           </ul>
         </div>
         <div className="flex flex-col gap-4 text-center">
           <div>
-            <h4 className="text-7xl font-bold">$19</h4>
+            <h4 className="text-7xl font-bold">$0</h4>
             <p className="text-sm font-medium text-muted-foreground">
               Billed Monthly
             </p>
@@ -58,10 +60,84 @@ export default function PricingPage() {
           </Link>
         </div>
       </div>
+
+      <div className="grid w-full items-start gap-10 rounded-lg border p-10 md:grid-cols-[1fr_200px]">
+        <div className="grid gap-6">
+          <h3 className="text-xl font-bold sm:text-2xl">
+            What&apos;s included in EMBLOY SMART
+          </h3>
+          <ul className="grid gap-3 text-sm text-muted-foreground sm:grid-cols-2">
+            <li className="flex items-center">
+              <Icons.check className="mr-2 h-4 w-4" /> Unlimited job postings
+            </li>
+            <li className="flex items-center">
+              <Icons.check className="mr-2 h-4 w-4" /> Unlimited client tokens
+            </li>
+
+            <li className="flex items-center">
+              <Icons.check className="mr-2 h-4 w-4" /> Client token customization
+            </li>
+            <li className="flex items-center">
+              <Icons.check className="mr-2 h-4 w-4" /> No ads
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-4 text-center">
+          <div>
+            <h4 className="text-7xl font-bold">$50</h4>
+            <p className="text-sm font-medium text-muted-foreground">
+              Billed Monthly
+            </p>
+          </div>
+          <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
+            Get Started
+          </Link>
+        </div>
+      </div>
+
+      <div className="grid w-full items-start gap-10 rounded-lg border p-10 md:grid-cols-[1fr_200px]">
+        <div className="grid gap-6">
+          <h3 className="text-xl font-bold sm:text-2xl">
+            What&apos;s included in EMBLOY GENIUS
+          </h3>
+          <ul className="grid gap-3 text-sm text-muted-foreground sm:grid-cols-2">
+            <li className="flex items-center">
+              <Icons.check className="mr-2 h-4 w-4" /> G...
+            </li>
+            <li className="flex items-center">
+              <Icons.check className="mr-2 h-4 w-4" /> E...
+            </li>
+
+            <li className="flex items-center">
+              <Icons.check className="mr-2 h-4 w-4" /> N...
+            </li>
+            <li className="flex items-center">
+              <Icons.check className="mr-2 h-4 w-4" /> I...
+            </li>
+            <li className="flex items-center">
+              <Icons.check className="mr-2 h-4 w-4" /> U...
+            </li>
+            <li className="flex items-center">
+              <Icons.check className="mr-2 h-4 w-4" /> S...
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-4 text-center">
+          <div>
+            <h4 className="text-7xl font-bold">?</h4>
+            <p className="text-sm font-medium text-muted-foreground">
+              Billed Monthly
+            </p>
+          </div>
+          <Link href="/login"   className={cn(buttonVariants({ size: "lg" }), 'pointer-events-none opacity-50')}>
+            Coming Soon ...
+          </Link>
+        </div>
+      </div>
+
       <div className="mx-auto flex w-full max-w-[58rem] flex-col gap-4">
         <p className="max-w-[85%] leading-normal text-muted-foreground sm:leading-7">
-          Embloy is a demo app.{" "}
-          <strong>You can test the upgrade and won&apos;t be charged.</strong>
+        <strong>Embloy</strong> partners with <strong>Stripe</strong> for simplified billing.
         </p>
       </div>
     </section>
