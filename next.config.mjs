2bd5ffe7a328bb0b980c66d48e4cad25272c99d6
carlobortolan <carlobortolan@gmail.com>
2024-02-18 16:37:08 +0100

    Update allowed hosts

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ import "./env.mjs"
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["avatars.githubusercontent.com", "f005.backblazeb2.com"],
+    domains: ["avatars.githubusercontent.com", "f005.backblazeb2.com", "embloy.blob.core.windows.net"],
   },
   experimental: {
     appDir: true,
