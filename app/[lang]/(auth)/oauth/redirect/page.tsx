c5f0e2ea0547e652fd3948687ae53d166e84a695
carlobortolan <carlobortolan@gmail.com>
2024-02-27 23:09:10 +0100

    Fix oauth bug

--- app/[lang]/(auth)/oauth/redirect/page.tsx
+++ app/[lang]/(auth)/oauth/redirect/page.tsx
@@ -8,24 +8,19 @@ import { getDictionary } from '@/app/[lang]/dictionaries';
 export default function OAuthRedirect({ params: { lang } }) {
   const refreshToken = useSearchParams().get('refresh_token');
   const [message, setMessage] = useState('Processing...');
-  const [dict, setDict] = useState<Record<string, any> | null>(null);
   
   useEffect(() => {
     const fetchUserAndDictionary = async () => {
       const dictionary = await getDictionary(lang);
-      setDict(dictionary);
-
-      if (dict) {
-        try {
-          const session = await getSession(refreshToken ?? undefined);
-          if (session.session) {
-            setMessage(dict.auth.oauth.success);
-          } else {
-            setMessage(dict.auth.oauth.error);
-          }
-        } catch (error) {
-          setMessage(dict.oauth.errorOccured.replace('{error}', error.message));
+      try {
+        const session = await getSession(refreshToken ?? undefined);
+        if (session.session) {
+          setMessage(dictionary.auth.oauth.success);
+        } else {
+          setMessage(dictionary.auth.oauth.error);
         }
+      } catch (error) {
+        setMessage(dictionary.oauth.errorOccured.replace('{error}', error.message));
       }
     };
 
