2be6424014c7b559e00426cf0f694e960704a170
carlobortolan <carlobortolan@gmail.com>
2024-11-15 20:09:01 +0100

    Update job schema

--- types/job-schema.ts
+++ types/job-schema.ts
@@ -1,19 +1,36 @@
 import { z } from "zod"
+
 import { applicationOptionSchema } from "./application-schema"
 
 export const jobSchema = z.object({
   id: z.number(),
-  job_type: z.string().nullable(),
-  job_slug: z.string(),
-  job_type_value: z.number().nullable().optional(),
-  job_status: z.string().optional(),
-  activity_status: z.number().optional(),
   user_id: z.number().optional(),
-  referrer_url: z.string().optional().nullable(),
-  duration: z.number(),
-  code_lang: z.string().nullable(),
   title: z.string().nullable(),
   position: z.string().nullable(),
+  key_skills: z.string().nullable(),
+  job_type: z.string().nullable(),
+  job_slug: z.string(),
+  job_status: z.string().optional(),
+  activity_status: z.number().optional(),
+  referrer_url: z.string().nullable(),
+  salary: z.number().nullable(),
+  currency: z.string().nullable(),
+  euro_salary: z.number().nullable(),
+  start_slot: z.string().nullable(),
+  duration: z.number(),
+  code_lang: z.string().nullable(),
+  longitude: z.number(),
+  latitude: z.number(),
+  country_code: z.string().nullable(),
+  postal_code: z.string().nullable(),
+  city: z.string().nullable(),
+  address: z.string().nullable(),
+  view_count: z.number(),
+  applications_count: z.number(),
+  created_at: z.string(),
+  updated_at: z.string(),
+  job_notifications: z.string().optional(),
+  image_url: z.string().nullable().optional(),
   description: z
     .object({
       id: z.number(),
@@ -26,25 +43,6 @@ export const jobSchema = z.object({
     })
     .nullable()
     .optional(),
-  key_skills: z.string().nullable().optional(),
-  salary: z.number().nullable(),
-  euro_salary: z.number().nullable().optional(),
-  currency: z.string().nullable(),
-  start_slot: z.string().nullable(),
-  longitude: z.number(),
-  latitude: z.number(),
-  country_code: z.string().nullable(),
-  postal_code: z.string().nullable(),
-  city: z.string().nullable(),
-  address: z.string().nullable(),
-  view_count: z.number(),
-  created_at: z.string(),
-  updated_at: z.string(),
-  applications_count: z.number(),
-  job_notifications: z.string().optional(),
-  deleted_at: z.string().nullable().optional(),
-  application_options: z.array(applicationOptionSchema).optional(),
-  image_url: z.string().nullable().optional(),
   employer: z
     .object({
       employer_email: z.string().nullable(),
@@ -54,6 +52,7 @@ export const jobSchema = z.object({
     })
     .nullable()
     .optional(),
+  application_options: z.array(applicationOptionSchema).optional(),
 })
 
 export type Job = z.infer<typeof jobSchema>
